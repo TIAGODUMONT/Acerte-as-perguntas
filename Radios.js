58b74d6f62bdced424cbc3ac17239525ba23e7c3
TIAGO ROMEU DUMONT <66042328+TIAGODUMONT@users.noreply.github.com>
2022-04-27 17:49:34 -0300

    Add files via upload

---
+++ Radios.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Radios = ({ pergunta, options, onChange, value, id, active }) => {
+
+  if(active === false) return null
+  return (
+    <fieldset
+      style={{
+        padding: "2rem",
+        marginBottom: "1rem",
+        border: "2px solid #eee",
+      }}
+    >
+      <legend style={{ fontWeight: "bold" }}>{pergunta}</legend>
+      {options.map((option) => (
+        <label key={option} style={{ marginBotton: "1rem" }}>
+          <input
+            type="radio"
+            id={id}
+            checked={value === option}
+            value={option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </fieldset>
+  );
+};
+
+export default Radios;
